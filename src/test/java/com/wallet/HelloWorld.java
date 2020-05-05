ce04a2d89ba1d7cbafc2b741bb0cc1e4dddbdbb2
Mauricio <mauriciomatusmura@yahoo.com.br>
2020-05-04 22:21:59 -0300

    configuração inicial para os testes

---
+++ src/test/java/com/wallet/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.wallet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.annotation.security.RunAs;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+		assertEquals(1, 1);
+	}
+}
--- src/test/java/com/wallet/WalletApplicationTests.java
+++ src/test/java/com/wallet/WalletApplicationTests.java
@@ -1,9 +1,11 @@
 package com.wallet;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WalletApplicationTests {
 
 	@Test
